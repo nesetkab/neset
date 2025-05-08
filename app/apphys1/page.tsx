e2e16f7f42809c097ac13327f9d9f119d99db3c4
nesetkab <foulofdoom@gmail.com>
2025-05-07 20:00:25 -0600

    lmao

---
+++ app/apphys1/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Link } from "lucide-react";
+
+export default function Example() {
+  return (
+    <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+        <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+          <a href="https://drive.google.com/drive/folders/1QXnMBtWY3PxI6g0PO4-LsvoNiDeUtbt_" className="text-white underline" >ap physics frqs</a>
+        </div>
+      </div>
+    </div>
+  );
+}
